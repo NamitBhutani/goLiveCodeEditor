85f51bd5a89607b90f2bcbf0db70a0f85e8d3e98
Namit <namit.bhut@gmail.com>
2023-09-28 22:29:39 +0530

    add: login page ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title class="flex justify-center">goLiveCodeEditor</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<div class="flex justify-center">Edit away!</div>
+	</Card.Content>
+	<Card.Footer class="flex justify-center">
+		<a href="/login"><Button class="mx-2">Login</Button></a>
+		<a href="/register"><Button class="mx-2">Register</Button></a>
+	</Card.Footer>
+</Card.Root>
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title class="flex justify-center">Login</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<form action="">
+			<Label>Username:</Label>
+			<Input type="text" />
+			<Label>Password:</Label>
+			<Input type="password" />
+		</form>
+	</Card.Content>
+	<Card.Footer class="flex justify-center">
+		<Button>Login</Button>
+	</Card.Footer>
+</Card.Root>
