04a1dc1a458b91b022f96dbacacf69de9f949f3b
Namit <namit.bhut@gmail.com>
2023-09-29 00:10:16 +0530

    add: backend code for login

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -3,6 +3,7 @@
 	import { Label } from '$lib/components/ui/label';
 	import * as Card from '$lib/components/ui/card';
 	import { Button } from '$lib/components/ui/button';
+	import { enhance } from '$app/forms';
 </script>
 
 <Card.Root>
@@ -10,14 +11,17 @@
 		<Card.Title class="flex justify-center">Login</Card.Title>
 	</Card.Header>
 	<Card.Content>
-		<form action="">
-			<Label>Username:</Label>
-			<Input type="text" />
-			<Label>Password:</Label>
-			<Input type="password" />
-		</form>
+		<div class="flex justify-center items-center mt-10">
+			<form action="?/login" method="POST" use:enhance>
+				<Label>Username:</Label>
+				<Input type="text" name="username" id="username" />
+				<Label>Password:</Label>
+				<Input type="password" name="password" id="password" />
+				<div class="flex justify-center mt-2"><Button>Login</Button></div>
+			</form>
+		</div>
 	</Card.Content>
 	<Card.Footer class="flex justify-center">
-		<Button>Login</Button>
+		<a href="/"><Button>Home</Button></a>
 	</Card.Footer>
 </Card.Root>
